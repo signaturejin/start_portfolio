a55a4061cc6a90f5fbad88edbb85ffd7deb59041
jin <schoolyoung2m@naver.com>
2023-03-22 22:59:11 +0900

    사이트 버튼 추가

---
+++ js/popUp.js
@@ -0,0 +1,39 @@
+// pc버전 옵션
+let pcOption = "width=1920px, height=1080px, resizable=no";
+// 타블렛 버전 옵션
+let tabletOption = "width=1024px, height=768px, top=100px, left=300px, resizable=no";
+// 모바일 버전 옵션
+let mobileOption = "width=420px, height=740px, top=100px, left=500px, resizable=no";
+
+// 포트폴리오1
+let pcPopup1 = ()=>{
+    window.open("https://portfolio1-377406.du.r.appspot.com/","_blank", pcOption);
+}
+let tabletPopup1 = ()=>{
+    window.open("https://portfolio1-377406.du.r.appspot.com/","_blank", tabletOption);
+}
+let mobilePopup1 = ()=>{
+    window.open("https://portfolio1-377406.du.r.appspot.com/","_blank", mobileOption);
+}
+
+// 포트폴리오2
+let pcPopup2 = ()=>{
+    window.open("https://portfolio2-377408.du.r.appspot.com/","_blank", pcOption);
+}
+let tabletPopup2 = ()=>{
+    window.open("https://portfolio2-377408.du.r.appspot.com/","_blank", tabletOption);
+}
+let mobilePopup2 = ()=>{
+    window.open("https://portfolio2-377408.du.r.appspot.com/","_blank", mobileOption);
+}
+
+// 포트폴리오3
+let pcPopup3 = ()=>{
+    window.open("https://portfolio3-377408.du.r.appspot.com/","_blank", pcOption);
+}
+let tabletPopup3 = ()=>{
+    window.open("https://portfolio3-377408.du.r.appspot.com/","_blank", tabletOption);
+}
+let mobilePopup3 = ()=>{
+    window.open("https://portfolio3-377408.du.r.appspot.com/","_blank", mobileOption);
+}
